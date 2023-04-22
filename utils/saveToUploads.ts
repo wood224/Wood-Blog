40829d15bf9c972f6fc242c5f77210e5cf85c645
wood224 <610518780@qq.com>
2023-04-22 10:57:07 +0800

    改进上传文件功能模块

--- utils/saveToUploads.ts
+++ utils/saveToUploads.ts
@@ -3,15 +3,33 @@ const path = require('path');
 const util = require('util');
 const rename = util.promisify(fs.rename); //转为promise
 
+const allowedDirs = ['category', 'note'];
+const allowedSuffixes = ['jpg', 'png'];
+const maxSize = 2 * 1024 * 1024; //2MB
+
+//获取文件后缀名
 function getFileSuffix(str: string) {
   const i = str.lastIndexOf(".");
   const path = str.substring(i + 1, str.length);
   return path;
 }
 
-export async function saveToUploads(dirname: string, tempPath: string, name: string) {
-  if (dirname !== 'category' && dirname !== 'note') return ''
+//校验文件
+function checkFile(tempPath: string) {
   const suffix = getFileSuffix(tempPath);
+  if (!allowedSuffixes.includes(suffix)) {
+    return false;
+  }
+  const size = fs.statSync(tempPath).size;
+  if (size > maxSize) return false
+  return suffix;
+}
+
+//保存文件
+export async function saveToUploads(dirname: string, tempPath: string, name: string) {
+  if (!allowedDirs.includes(dirname)) return '';
+  const suffix = checkFile(tempPath);
+  if (!suffix) return '';
   const pathName = `${Date.now()}-${name}.${suffix}`;   //根据当前时间戳生成文件名
   const targetPath = path.join(__dirname, `../public/uploads/${dirname}`, `${pathName}`);
   try {
