c6db030f4277d957f1baad4dd368a2f043c098ba
wood224 <610518780@qq.com>
2023-05-26 21:42:17 +0800

    增加退出登录按钮

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -5,18 +5,37 @@
         <span>Wood's Blog</span>
       </div>
     </div>
-    <div class="right"></div>
+    <div class="right">
+      <el-button type="danger" @click="logout">退出登录</el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang='ts'>
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+
+const logout = () => {
+  ElMessageBox.confirm('确定退出登陆吗？', '提示', {
+    confirmButtonText: '确认',
+    confirmButtonClass: 'confirm-remove-button',
+    cancelButtonText: '取消',
+    type: 'warning',
+  })
+    .then(() => {
+      localStorage.removeItem('token');
+      router.push('/login');
+    })
+    .catch(() => { })
+}
 </script>
 
 <style scoped lang='scss'>
 .header-wrapper {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 10px 50px;
   border-bottom: 2px solid rgb(0, 255, 255);
   width: 100%;
