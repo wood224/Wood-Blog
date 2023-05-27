583b57cf0253a97a3035abb3380089c2f35ae649
wood224 <610518780@qq.com>
2023-05-27 17:30:25 +0800

    上传图片的接口文档

--- routes/upload.ts
+++ routes/upload.ts
@@ -6,6 +6,30 @@ import { uploadController } from '../controllers/uploadController';
  * @apiDefine groupCategory 上传
  */
 
+/**
+ * 
+ * @api {post} /upload 上传图片
+ * @apiGroup groupCategory
+ * @apiVersion  1.0.0
+ * 
+ * 
+ * @apiBody  {File} img 图片
+ * 
+ * @apiParamExample  {multipart/form-data} 请求示例:
+ * {
+ *    "img" : File
+ * }
+ * 
+ * 
+ * @apiSuccess {String} url  图片相对路径
+ * 
+ * @apiSuccessExample {json} 成功响应:
+ * { 
+ *    "url":"/path.png"
+ * }
+ * 
+ * 
+ */
 router.post('/', uploadController.upload);
 
 export default router;
